128645738360cfe2100c3960db33aaf409050274
hiroshi <hiroshi3110@gmail.com>
2024-10-14 16:19:58 +0900

    Redirect back to the app after logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
   def new
     # session['login'] = true
+    session[:origin] = request.referer
   end
 
   def create
-    # debugger
     auth = request.env['omniauth.auth']
     uid_provider = auth.slice(:provider, :uid)
     user = User.find_by(accounts: { '$elemMatch': uid_provider })
     user ||= User.create!(accounts: [uid_provider.merge(auth[:info].slice(:email))])
     session[:user_id] = user.id
 
-    render plain: 'logged in.'
+    origin = session[:origin]
+    session.delete(:origin)
+    # TODO: allow only registerd host
+    if origin
+      redirect_to origin, allow_other_host: true
+    else
+      render plain: 'logged in.'
+    end
   end
 end
