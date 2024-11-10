581acf0de967615d044c20b8914aa4b70834350e
hiroshi <hiroshi3110@gmail.com>
2024-11-10 09:42:00 +0900

    render sample react component.

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,7 +3,7 @@
 //
 //    <%= vite_client_tag %>
 //    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+console.log("Vite ⚡️ Rails");
 
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
@@ -11,7 +11,10 @@ console.log('Vite ⚡️ Rails')
 // If you want to use .jsx or .tsx, add the extension:
 //     <%= vite_javascript_tag 'application.jsx' %>
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+console.log(
+  "Visit the guide for more information: ",
+  "https://vite-ruby.netlify.app/guide/rails",
+);
 
 // Example: Load Rails libraries in Vite.
 //
@@ -26,3 +29,11 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+// https://github.com/ElMassimo/vite_ruby/blob/cfdb0bed7e365787622d66b3e90097a9c54dca70/examples/rails/app/frontend/entrypoints/example_import.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Hero from "~/components/Hero.jsx";
+
+const root = createRoot(document.getElementById("react-root"));
+root.render(React.createElement(Hero));
