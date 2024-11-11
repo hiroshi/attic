23689f4b2fb62224cb533f38c78cbee5ad6418e7
hiroshi <hiroshi3110@gmail.com>
2024-11-11 15:44:10 +0900

    fix the hmr problem.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [RubyPlugin(), react()],
+  server: {
+    hmr: {
+      host: "vite.lvh.me",
+    },
+  },
 });
